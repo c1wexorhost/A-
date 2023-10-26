bdf017b058182388fba736d37159ad7e350b79f5
shihar <shiharbinsideeque73@gmail.com>
2023-10-26 17:26:59 +0530

    sub manu added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//   // make it as accordion for smaller screens
+//   if (window.innerWidth < 992) {
+//     console.log("sdosudfhuskfdjkhjhkkjkk");
+//     // close all inner dropdowns when parent is closed
+//     document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+//       everydropdown.addEventListener('hidden.bs.dropdown', function () {
+//         // after dropdown is hidden, then find all submenus
+//           this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+//             // hide every submenu as well
+//             // everysubmenu.style.display = 'none';
+//           });
+//       })
+//     });
+  
+//     document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+//       element.addEventListener('click', function (e) {
+//           let nextEl = this.nextElementSibling;
+//           if(nextEl && nextEl.classList.contains('submenu')) {	
+//             // prevent opening link if link needs to open dropdown
+//             e.preventDefault();
+//             if(nextEl.style.display == 'block'){
+//               nextEl.style.display = 'none';
+//             } else {
+//               nextEl.style.display = 'block';
+//             }
+  
+//           }
+//       });
+//     })
+//   }
+//   // end if innerWidth
+//   }); 
+
+
+// Enable mobile-friendly behavior for submenus
+document.querySelectorAll('.dropdown-menu .dropdown-submenu').forEach(function(element) {
+  element.addEventListener('click', function(event) {
+    event.stopPropagation();
+    if (window.innerWidth <= 991) {
+      this.querySelector('.dropdown-menu').classList.toggle('show');
+    }
+  });
+});
+
+
 (function ($) {
   "use strict";
   var spinner = function () {
